83e15d89e2f32bc6a94ebc2e0eb124c593d21ddb
tim <22520614+TimouZ@users.noreply.github.com>
2020-02-17 12:19:47 +0200

    Syntax fixes

--- rose_cassidy_mov.py
+++ rose_cassidy_mov.py
@@ -1,10 +1,13 @@
+import time
+from os import system
+
 def create_slowmo_movie():
     print('Converting to film now')
-    system('avconv -r 24 -i image%04d.jpg -vcodec libx264 -crf 20 -g 15 'date +%H%M%%d%m%Y'timelapse.mp4')
+    system('avconv -r 24 -i image%04d.jpg -vcodec libx264 -crf 20 -g 15 `date +%H%M%%d%m%Y`timelapse.mp4')
 
     #create film
     print('moving completed mp4 file')
-    system('mv *.mp4 ~/timelapse/completed/')
+    system('mv *.mp4 ~/rose_cassidy/completed/')
 
     print('cleaning up old jpgs')
     system('rm *.jpg')
