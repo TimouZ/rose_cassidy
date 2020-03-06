ca4669519446658992e61340863ef52d1fe8acaa
Tim Uzlov <22520614+TimouZ@users.noreply.github.com>
2020-03-06 21:28:49 +0200

    Some error handling has been added

--- rose_cassidy_run.py
+++ rose_cassidy_run.py
@@ -1,39 +1,55 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import time
 from os import system
+from os import path
 
 from picamera import PiCamera
 import configparser
 
 from rose_cassidy_mov import create_slowmo_movie
 
+CONFIG_FILE_NAME = 'rose_cassidy.ini'
+
+
 def init_data(config_file_name):
     global SLEEP_TIME
-    global START_FRAME
-    global STOP_FRAME
-    global CAM_RESOLUTION
+    global FRAME_COUNT
+    global CAMERA_RESOLUTION
 
     config = configparser.ConfigParser()
-    config.read(config_file_name)
-    #CAM_RESOLUTION = tuple(config['DEFAULT']['Resolution'])
-    SLEEP_TIME = int(config['DEFAULT']['SleepTime'])
-    START_FRAME = int(config['DEFAULT']['FrameCount'])
-    STOP_FRAME = int(config['DEFAULT']['FrameStop'])
+    
+    try: 
+        with open(config_file_name) as config_file:
+            config.read_file(config_file)
+           
+            #CAMERA_RESOLUTION = config.get('camera_settings','Resolution')
+            SLEEP_TIME = int(config.get('camera_settings', 'SleepTime'))
+            FRAME_COUNT = int(config.get('camera_settings','FrameCount'))
+            
+    except Exception as e:
+        print('Can`t read config file {}, error {} occured '.format(config_file_name, e))
+        
 
 def run_cam():
 
     camera = PiCamera()
-    init_data('rose_cassidy.ini')
-    #camera.resolution = CAM_RESOLUTION
+    init_data(CONFIG_FILE_NAME)
+    #camera.resolution(CAMERA_RESOLUTION)
 
-    print('Photography process will take approximately ', str(int(STOP_FRAME) * int(SLEEP_TIME) / 60), ' minutes')
+    print('Photography process will take approximately ', str(int(FRAME_COUNT) * int(SLEEP_TIME) / 60), ' minutes')
     print('Taking photos now')
 
-
-    for frame in range(STOP_FRAME):
-        print('Picture: ' + str(frame) + ' of ' + str(STOP_FRAME))
-        camera.capture('image' + str(frame).zfill(4) + '.jpg')
-        time.sleep(SLEEP_TIME)
+    try:
+        for frame in range(FRAME_COUNT):
+            print('Picture: ' + str(frame) + ' of ' + str(FRAME_COUNT))
+            camera.capture('image' + str(frame).zfill(4) + '.jpg')
+            time.sleep(SLEEP_TIME)
+    except Exception as e:
+        pass
+    else:
+        print('No errors occured during the photographing process.')
+    finally:
+        camera.close()
 
 if __name__ == '__main__':
     run_cam()
