121a924ef5dadb8b35fb53aff6ac5f8603fb794b
Tim Uzlov <22520614+TimouZ@users.noreply.github.com>
2020-03-07 00:31:52 +0200

    Refactoring

--- rose_cassidy_mov.py
+++ rose_cassidy_mov.py
@@ -1,18 +1,41 @@
+#!/usr/bin/python3
 import time
+import os
+import configparser
 from os import system
 
+CONFIG_FILE_NAME = 'rose_cassidy.ini'
+
+
+def init_data(config_file_name):
+    global MOVIE_DIR
+    config = configparser.ConfigParser()   
+    try: 
+        with open(config_file_name) as config_file:
+            config.read_file(config_file)
+            MOVIE_DIR = config.get('environment_settings','Movie_dir')
+    except Exception as e:
+        print('Can`t read config file {}, error {} occured '.format(config_file_name, e))
+
+
 def create_slowmo_movie():
-    print('Converting to film now')
+    init_data(CONFIG_FILE_NAME)    
+    
+    print('Converting photos to film now')
     system('ffmpeg -r 24 -i image%04d.jpg -vcodec libx264 -crf 20 -g 15 `date +%Y%m%d%H%M`timelapse.mp4')
 
-
-    #create film
-    print('moving completed mp4 file')
+     #Checking directory and creating directory
+    if not os.path.exists(MOVIE_DIR):
+        print('Can`t find dir for movie, creating...')
+        try: 
+            os.mkdir(MOVIE_DIR)
+        except Exception as e:
+            print('Can`t create dir for movie, error {} occured '.format(e))
+ 
+    print('Moving completed mp4 file')
     system('mv *.mp4 ~/rose_cassidy/completed/')
 
-
-
-    print('cleaning up old jpgs')
+    print('Cleaning up old jpgs')
     system('rm *.jpg')
 
     print('Done')
--- rose_cassidy_run.py
+++ rose_cassidy_run.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 import time
-from os import system
-from os import path
-
+import os
 from picamera import PiCamera
 import configparser
-
 from rose_cassidy_mov import create_slowmo_movie
 
 CONFIG_FILE_NAME = 'rose_cassidy.ini'
@@ -15,29 +12,25 @@ def init_data(config_file_name):
     global SLEEP_TIME
     global FRAME_COUNT
     global CAMERA_RESOLUTION
-
-    config = configparser.ConfigParser()
-    
+    global MOVIE_DIR
+    config = configparser.ConfigParser()   
     try: 
         with open(config_file_name) as config_file:
             config.read_file(config_file)
-           
             #CAMERA_RESOLUTION = config.get('camera_settings','Resolution')
-            SLEEP_TIME = int(config.get('camera_settings', 'SleepTime'))
-            FRAME_COUNT = int(config.get('camera_settings','FrameCount'))
-            
+            SLEEP_TIME = int(config.get('photos_settings', 'SleepTime'))
+            FRAME_COUNT = int(config.get('photos_settings','FrameCount'))
     except Exception as e:
         print('Can`t read config file {}, error {} occured '.format(config_file_name, e))
-        
+       
 
 def run_cam():
-
     camera = PiCamera()
     init_data(CONFIG_FILE_NAME)
     #camera.resolution(CAMERA_RESOLUTION)
 
     print('Photography process will take approximately ', str(int(FRAME_COUNT) * int(SLEEP_TIME) / 60), ' minutes')
-    print('Taking photos now')
+    print('Taking photos now...')
 
     try:
         for frame in range(FRAME_COUNT):
@@ -45,7 +38,7 @@ def run_cam():
             camera.capture('image' + str(frame).zfill(4) + '.jpg')
             time.sleep(SLEEP_TIME)
     except Exception as e:
-        pass
+        print('Can`t take photos, error {} occured '.format(e))
     else:
         print('No errors occured during the photographing process.')
     finally:
