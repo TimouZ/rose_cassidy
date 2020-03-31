bcaf83460862c6b72bab9299c1ff38fc93ec0581
tim <22520614+TimouZ@users.noreply.github.com>
2020-03-31 17:33:20 +0300

    config parser functions added

--- helpers.py
+++ helpers.py
@@ -3,7 +3,6 @@
 A module for helper functions
 """
 
-
 import os
 import configparser
 
@@ -14,16 +13,17 @@ def create_config(path):
     :return:
     """
     config = configparser.ConfigParser()
-    config.add_section('camera_settings')
-    config.set('Resolution', '1920, 1080')
-    config.add_section('photos_settings')
-    config.set('SleepTime', '1')
-    config.set('FrameCount', '5')
-    config.add_section('environment_settings')
-    config.set('Movie_dir', '/movie ready/')
+    config.add_section("camera_settings")
+    config.set("Resolution", "1920, 1080")
+    config.add_section("photos_settings")
+    config.set("SleepTime", "1")
+    config.set("FrameCount", "5")
+    config.add_section("environment_settings")
+    config.set("Movie_dir", "/movie ready/")
 
-    with open(path, 'w') as config_file:
+    with open(path, "w") as config_file:
         config.write(config_file)
+
 
 def get_config(path):
     """
@@ -39,6 +39,7 @@ def get_config(path):
         config.read_file(config_file)
     return config
 
+
 def get_setting(path, section, setting):
     """
     :param path:
@@ -48,5 +49,33 @@ def get_setting(path, section, setting):
     """
     config = get_config(path)
     value = config.get(section, setting)
-    msg = "Get {section} {setting}".format(section, setting)
+    message = "Get {section} {setting}".format(section=section, setting=setting)
+    print(message)
     return value
+
+
+def update_setting(path, section, setting, value):
+    """
+    :param path:
+    :param section:
+    :param setting:
+    :param value:
+    :return:
+    """
+    config = get_config(path)
+    config.set(section, setting, value)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
+def delete_setting(path, section, setting):
+    """
+    :param path:
+    :param section:
+    :param setting:
+    :return:
+    """
+    config = get_config(path)
+    config.remove_option(section, setting)
+    with open(path, "w") as config_file:
+        config.write(config_file)
