04ccd006a294570884a9649e87804d0a08436490
tim <22520614+TimouZ@users.noreply.github.com>
2020-03-31 21:00:40 +0300

    Settings bug fixed

--- rose_cassidy_mov.py
+++ rose_cassidy_mov.py
@@ -8,22 +8,22 @@ import helpers
 CONFIG_FILE_NAME = 'rose_cassidy.ini'
 
 def create_timelapse_movie():
-    helpers.get_setting(CONFIG_FILE_NAME, 'Environment', 'movie_dir')
+    movie_dir = helpers.get_setting(CONFIG_FILE_NAME, 'Environment', 'movie_dir')
     
     print('-------Converting photos to movie now-------')
     system('ffmpeg -r 24 -i image%04d.jpg -vcodec libx264 -crf 20 -g 15 `date +%Y%m%d%H%M`timelapse.mp4')
     print('----------------Movie ready-----------------')
     
      #Checking directory and creating directory
-    if not os.path.exists(MOVIE_DIR):
+    if not os.path.exists(movie_dir):
         print('Can`t find dir for movie, creating...')
         try: 
-            os.mkdir(MOVIE_DIR)
+            os.mkdir(movie_dir)
         except Exception as e:
             print('Can`t create dir for movie, error {} occured '.format(e))
  
     print('Moving completed mp4 file')
-    system('mv *.mp4 {}'.format(MOVIE_DIR))
+    system('mv *.mp4 {}'.format(movie_dir))
 
     print('Cleaning up old jpgs')
     system('rm *.jpg')
